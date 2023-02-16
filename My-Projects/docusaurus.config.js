f439134e2a2655b9dca30408105d25de73ece6e6
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-02-16 12:06:53 +0530

    updated config file

--- My-Projects/docusaurus.config.js
+++ My-Projects/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Projects ',
   tagline: 'All the projects I am building ',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/innovation.png',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
